075d787b0ebf112ec6c8814c2338833385e955d4
Kaelan Davis <kaeland2@illinois.edu>
2022-09-05 19:42:51 -0500

    Forgot to commit, implemented destroy, working through memory errors

--- extreme_edge_cases/camelCaser.c
+++ extreme_edge_cases/camelCaser.c
@@ -4,13 +4,112 @@
  */
 #include "camelCaser.h"
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <assert.h>
+#include <errno.h>
+#include <stdio.h>
+
+char * camel_case_sentence(const char * sentence);
+size_t get_sentence_count(const char * src);
+void copy_chars(char * dest, const char * src, size_t length);
+void handle_punct(size_t idx, char ** output, const char * input_str, const char * word_start);
 
 char **camel_caser(const char *input_str) {
-    // TODO: Implement me!
-    return NULL;
+    //If input is null return null
+    if (!input_str) return NULL;
+
+    char ** output = NULL;
+    const char * itr = input_str;
+    size_t count = get_sentence_count(itr);
+
+    output = (char **) malloc((count + 1) * sizeof(char **));
+    output[count] = NULL;
+
+    const char * word_start = input_str;
+
+    size_t idx = 0;
+    while (*input_str) {
+        if (ispunct(*input_str)) {
+            handle_punct(idx, output, input_str, word_start);
+            idx++;
+            word_start = (input_str + 1);
+        }
+        input_str++;
+    }
+
+    return output;
 }
 
 void destroy(char **result) {
     // TODO: Implement me!
+    char ** inc = result;
+
+    while (*inc) {
+        char * temp = *inc;
+        free(temp);
+        inc++;
+    }
+
+    free(result);
     return;
+}
+
+void handle_punct(size_t idx, char ** output, const char * input_str, const char * word_start) {
+    size_t length = input_str - word_start;
+    char * camel_cased = NULL;
+
+    if (length) {
+        char * sentence = (char *) malloc(length + 1);
+
+        if (!sentence) {
+            perror("malloc failed!");
+            exit(1);
+        }
+
+        sentence[length] = '\0';
+        copy_chars(sentence, word_start, length);
+
+        camel_cased = camel_case_sentence(sentence);
+
+        free(sentence);
+    } else {
+        camel_cased = "";
+    }
+
+    output[idx] = camel_cased;
+}
+
+size_t get_sentence_count(const char * src) {
+    size_t count = 0;
+    
+    while(*src) {
+        if (ispunct(*src)) {
+            count++;
+        }
+
+        src++;
+    }
+
+    return count;
+}
+
+void copy_chars(char * dest, const char * src, size_t length) {
+    size_t i = 0;
+
+    for ( ; i < length; i++) {
+        dest[i] = src[i];
+    }
+}
+
+char * camel_case_sentence(const char * sentence) {
+    if (!sentence) {
+        return "";
+    }
+
+    char * camel_cased = (char *) malloc(strlen(sentence) + 1);
+    camel_cased[strlen(sentence)] = '\0';
+    strcpy(camel_cased, sentence);
+
+    return camel_cased;
 }
--- extreme_edge_cases/camelCaser_tests.c
+++ extreme_edge_cases/camelCaser_tests.c
@@ -5,12 +5,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 #include "camelCaser.h"
 #include "camelCaser_tests.h"
 
+int compare_output(char ** actual, char ** expected);
+void print_camelcase(char ** to_print);
+
 int test_camelCaser(char **(*camelCaser)(const char *),
                     void (*destroy)(char **)) {
     // TODO: Implement me!
+    char * input1 = "Hello.World4 test.not here";
+    char * expected1[] = {"hello", "world4Test", NULL};
+
+    char ** actual1 = camelCaser(input1);
+    print_camelcase(actual1);
+    print_camelcase(expected1);
+    assert(compare_output(actual1, expected1));
+    destroy(actual1);
+
     return 1;
+}
+
+int compare_output(char ** actual, char ** expected)  {
+    while (*actual && *expected) {
+        int compare = strcmp(*actual, *expected);
+
+        if (!compare) {
+            return 0;
+        }
+
+        actual++;
+        expected++;
+    }
+
+    return (*actual == NULL && *expected == NULL);
+}
+
+void print_camelcase(char ** to_print) {
+    do {
+        printf("%s\n", *to_print);
+    } while (*to_print++);
+    printf("\n");
 }
