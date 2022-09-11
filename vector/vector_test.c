3f3cd21730dc301ee829f0413ce0a0849b51c268
Kaelan Davis <kaeland2@illinois.edu>
2022-09-10 20:46:17 -0500

    Added some simple init tests

--- vector/vector_test.c
+++ vector/vector_test.c
@@ -3,7 +3,18 @@
  * CS 341 - Fall 2022
  */
 #include "vector.h"
+
+#include <assert.h>
+
 int main(int argc, char *argv[]) {
     // Write your test cases here
+    vector * test_vector_shallow = vector_create(NULL, NULL, NULL);
+
+    // Test vector init values
+    assert(test_vector_shallow);
+    assert(vector_size(test_vector_shallow) == 0);
+    assert(vector_capacity(test_vector_shallow) == 8);
+    vector_destroy(test_vector_shallow);
+
     return 0;
 }
