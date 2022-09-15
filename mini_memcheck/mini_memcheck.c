47f972b186437cfa8ae093416f12a6a847cf8975
Kaelan Davis <kaeland2@illinois.edu>
2022-09-15 15:07:05 -0500

    Implemented mini-realloc

--- mini_memcheck/mini_memcheck.c
+++ mini_memcheck/mini_memcheck.c
@@ -3,14 +3,18 @@
  * CS 341 - Fall 2022
  */
 #include "mini_memcheck.h"
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 
 void *mini_malloc(size_t request_size, const char *filename,
                   void *instruction) {
-    total_memory_requested += request_size;
-
     meta_data * node = malloc(sizeof(meta_data) + request_size);
+    if (!node) {
+        return NULL;
+    }
+
+    total_memory_requested += request_size;
     node->request_size = request_size;
     node->filename = filename;
     node->instruction = instruction;
@@ -23,9 +27,12 @@ void *mini_malloc(size_t request_size, const char *filename,
 void *mini_calloc(size_t num_elements, size_t element_size,
                   const char *filename, void *instruction) {
     size_t amount_requested = (num_elements * element_size);
-    total_memory_requested += amount_requested;
-
     meta_data * node = malloc(sizeof(meta_data) + amount_requested);
+    if (!node) {
+        return NULL;
+    }
+
+    total_memory_requested += amount_requested;
     node->request_size = amount_requested;
     node->filename = filename;
     node->instruction = instruction;
@@ -39,10 +46,60 @@ void *mini_calloc(size_t num_elements, size_t element_size,
 
 void *mini_realloc(void *payload, size_t request_size, const char *filename,
                    void *instruction) {
-    void * temp = realloc
-    return NULL;
+    if (!_check_valid_address(payload)) {
+        invalid_addresses++;
+        return NULL;
+    }
+
+    total_memory_requested += request_size
+    void * temp = realloc(payload, sizeof(meta_data) + request_size);
+    if (!temp) {
+        return NULL;
+    }
+
+    if (payload != temp) {
+        // Memory was moved, restore the link
+        meta_data * start = head;
+        while (start) {
+            if (start->next == payload) {
+                start->next = temp;
+                break;
+            }
+
+            start = start->next;
+        }
+
+        // Do not need anything here because payload will always be found, guarenteed by _check_valid_address
+    }
+    
+    payload = temp;
+    (meta_data *) payload->request_size = request_size;
+    (meta_data *) payload->filename = filename;
+    (meta_data *) payload->instruction = instruction;
+    // no need to edit this nodes place in the linked list
+
+    return payload;
 }
 
 void mini_free(void *payload) {
     // your code here
+}
+
+// Checks to see if the input payload is a valid address to free in the linked list
+bool _check_valid_address(void *payload) {
+    if (!payload) {
+        // NULL is always a valid address to realloc or free
+        return true;
+    }
+
+    memset * start = head;
+    while (start) {
+        if (start == payload) {
+            return true;
+        }
+
+        start = start->next;
+    }
+
+    return false;
 }
