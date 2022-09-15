10d908a3e869ae4302751dadb80ac743684b9c73
Kaelan Davis <kaeland2@illinois.edu>
2022-09-15 14:44:00 -0500

    Added untested implementations of mini_malloc and mini_calloc

--- mini_memcheck/mini_memcheck.c
+++ mini_memcheck/mini_memcheck.c
@@ -8,19 +8,38 @@
 
 void *mini_malloc(size_t request_size, const char *filename,
                   void *instruction) {
-    // your code here
-    return NULL;
+    total_memory_requested += request_size;
+
+    meta_data * node = malloc(sizeof(meta_data) + request_size);
+    node->request_size = request_size;
+    node->filename = filename;
+    node->instruction = instruction;
+    node->next = head;
+
+    head = node;
+    return node;
 }
 
 void *mini_calloc(size_t num_elements, size_t element_size,
                   const char *filename, void *instruction) {
-    // your code here
-    return NULL;
+    size_t amount_requested = (num_elements * element_size);
+    total_memory_requested += amount_requested;
+
+    meta_data * node = malloc(sizeof(meta_data) + amount_requested);
+    node->request_size = amount_requested;
+    node->filename = filename;
+    node->instruction = instruction;
+    node->next = head;
+
+    memset(node + sizeof(meta_data), 0, amount_requested)
+
+    head = node;
+    return node;
 }
 
 void *mini_realloc(void *payload, size_t request_size, const char *filename,
                    void *instruction) {
-    // your code here
+    void * temp = realloc
     return NULL;
 }
 
