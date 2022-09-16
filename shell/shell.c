ea4d899a7461bbd0fd69d7227f730a0563a4f0b8
Kaelan Davis <kaeland2@illinois.edu>
2022-09-16 16:59:03 -0500

    Implemented simple shell prompting and taking user input

--- shell/shell.c
+++ shell/shell.c
@@ -5,6 +5,9 @@
 #include "format.h"
 #include "shell.h"
 #include "vector.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <unistd.h>
 
 typedef struct process {
     char *command;
@@ -13,5 +16,32 @@ typedef struct process {
 
 int shell(int argc, char *argv[]) {
     // TODO: This is the entry point for your shell.
+    pid_t shell_pid = getpid();
+    char * cwd = NULL;
+    char * input_buffer = NULL;
+    size_t input_size = 0;
+
+    while(true) {
+        // Get the current working directory
+        free(cwd);
+        cwd = get_full_path(".");
+
+        // Print shell prompt line
+        print_prompt(cwd, shell_pid);
+
+        // Read the user input and check to make sure it worked
+        free(input_buffer);
+        input_buffer = NULL;
+        input_size = 0;
+        ssize_t chars = getline(&input_buffer, &input_size, stdin);
+        if (chars == -1) {
+            // getline failed
+            return 1;
+        } else if (chars > 0 && input_buffer[chars - 1] == '\n') {
+            // replace the newline character with char NUL
+            input_buffer[chars - 1] = '\0';
+        }
+
+    }
     return 0;
 }
