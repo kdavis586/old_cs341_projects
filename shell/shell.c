c6a60d8a299c8bcbce5928afaf08499f0efad079
Kaelan Davis <kaeland2@illinois.edu>
2022-09-16 21:50:12 -0500

    Added global variables to allow for helper cleanup function

--- shell/shell.c
+++ shell/shell.c
@@ -10,47 +10,64 @@
 #include <string.h>
 #include <unistd.h>
 
+// Forward declare helper functions
+ssize_t _get_input(char ** buffer, size_t * size);
+void _cleanup();
+
 typedef struct process {
     char *command;
     pid_t pid;
 } process;
 
+// Globals that will be used for the duration of the shell
+static char * CWD;
+static char * INPUT_BUFFER;
+static size_t INPUT_SIZE;
+static vector * CMD_HIST;
+
 int shell(int argc, char *argv[]) {
-    // TODO: This is the entry point for your shell.
+    // Init variables that will be used for the lifetime of the shell
     pid_t shell_pid = getpid();
-    char * cwd = NULL;
-    char * input_buffer = NULL;
-    size_t input_size = 0;
+    CWD = get_full_path(".");
+    CMD_HIST = vector_create(&string_copy_constructor, &string_destructor, &string_default_constructor);
 
     while(true) {
-        // Get the current working directory
-        free(cwd);
-        cwd = get_full_path(".");
-
         // Print shell prompt line
-        print_prompt(cwd, shell_pid);
+        print_prompt(CWD, shell_pid);
 
         // Read the user input and check to make sure it worked
-        free(input_buffer);
-        input_buffer = NULL;
-        input_size = 0;
-        ssize_t chars = getline(&input_buffer, &input_size, stdin);
+        ssize_t chars = _get_input(&INPUT_BUFFER, &INPUT_SIZE);
         if (chars == -1) {
             // getline failed
-            free(cwd);
-            free(input_buffer);
+            _cleanup();
             return 1;
-        } else if (chars > 0 && input_buffer[chars - 1] == '\n') {
+        } else if (chars > 0 && INPUT_BUFFER[chars - 1] == '\n') {
             // replace the newline character with char NUL
-            input_buffer[chars - 1] = '\0';
+            INPUT_BUFFER[chars - 1] = '\0';
+
+            // Add the input into a history vector
+            vector_push_back(CMD_HIST, INPUT_BUFFER);
         }
 
-        if (strcmp(input_buffer, "exit") == 0) {
-            free(cwd);
-            free(input_buffer);
+        if (strcmp(INPUT_BUFFER, "exit") == 0) {
+            // exit was input, free memory and exit the shell.
+             _cleanup();
             break;
         }
 
     }
     return 0;
+}
+
+ssize_t _get_input(char ** buffer, size_t * size) {
+    free(*buffer);
+    *buffer = NULL;
+    *size = 0;
+    return getline(buffer, size, stdin);
+}
+
+void _cleanup() {
+    free(CWD);
+    free(INPUT_BUFFER);
+    vector_destroy(CMD_HIST);
 }
