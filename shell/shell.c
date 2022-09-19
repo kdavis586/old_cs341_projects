7450e7a1ebf73e0a902897fd2aff6b33b195d3cf
Kaelan Davis <kaeland2@illinois.edu>
2022-09-19 16:34:51 -0500

    Got writing to history file to work again.

--- shell/shell.c
+++ shell/shell.c
@@ -23,14 +23,15 @@ bool _parse_arguments(int argc, char * argv[]);
 bool _validate_options(int argc, char * argv[], char ** hist_path, char ** script_path);
 bool _run_builtin(vector * args);
 void _print_history();
-char * _run_prefix(char * prefix);
-char * _run_command(vector * args);
 vector * _split_input(char * command);
 void _handle_get_input(ssize_t chars);
 void _exit_success();
 void _handle_exit();
 bool _handle_history(vector * args);
 bool _handle_prev_command(vector * args);
+bool _handle_cd(vector * args);
+bool _handle_prefix();
+void _add_to_history(char * command);
 
 // Define process struct
 typedef struct process {
@@ -48,6 +49,7 @@ static bool RUN_SCRIPT;
 static FILE * HISTORY_FILE; // File for logging the history (if -h)
 static FILE * SCRIPT_FILE;  // File to run commands from (if -f)
 static FILE * INPUT_STREAM; // Used to store the stream to read commands from
+static ssize_t CHARS_READ;
 
 
 /*
@@ -59,26 +61,25 @@ int shell(int argc, char *argv[]) {
 
     while(true) {
         print_prompt(CWD, shell_pid);
-        ssize_t chars = _get_input(&INPUT_BUFFER, &INPUT_SIZE);
-        _handle_get_input(chars);
+        CHARS_READ = _get_input(&INPUT_BUFFER, &INPUT_SIZE);
+        _handle_get_input(CHARS_READ);
 
         // chars guarenteed to be >= 0
-        if (chars > 0) {
+        if (CHARS_READ > 0) {
             // Format for running command
-            if (INPUT_BUFFER[chars - 1] == '\n') {
-                INPUT_BUFFER[chars - 1] = '\0';
+            if (INPUT_BUFFER[CHARS_READ - 1] == '\n') {
+                INPUT_BUFFER[CHARS_READ - 1] = '\0';
             }
             _handle_exit();
 
             
             vector * command_args = _split_input(INPUT_BUFFER); // used for builtin
-            // TODO: get rid of this
-            vector_size(command_args);
+            _run_builtin(command_args);
             
             if (RUN_SCRIPT) {
                 print_command(INPUT_BUFFER);
             }
-            //vector_destroy(command_args) // TODO: Very bad to free every call, use same vector and clear
+            vector_destroy(command_args); // TODO: Very bad to free every call, use same vector and clear
         }
 
     }
@@ -96,9 +97,17 @@ int shell(int argc, char *argv[]) {
 //  NOTE: All functions used either do not cause errors (according to man) or are given functions
 void _shell_setup(pid_t * shell_pid, int argc, char * argv[]) {
     opterr = 0; // No error reporting on getopt
-    INPUT_STREAM = stdin;
     CWD = get_full_path(".");
+    INPUT_BUFFER = NULL;
+    INPUT_SIZE = 0;
     CMD_HIST = string_vector_create();
+    RECORD_HIST = false;
+    RUN_SCRIPT = false;
+    HISTORY_FILE = NULL;
+    SCRIPT_FILE = NULL;
+    INPUT_STREAM = stdin;
+    CHARS_READ = -1;
+    
     *shell_pid = getpid();
 
     if (!_parse_arguments(argc, argv)) {
@@ -128,9 +137,13 @@ bool _parse_arguments(int argc, char * argv[]) {
         return false;
     }
 
-    if (hist_path && !(HISTORY_FILE = fopen(hist_path, "a"))) {
-        print_history_file_error();
-        return false;
+    if (hist_path) {
+        fprintf(stderr, "path: %s\n", hist_path);
+        HISTORY_FILE = fopen(hist_path, "a");
+        if (!HISTORY_FILE) {
+            print_history_file_error();
+            return false;
+        }
     }
     
     if (script_path && !(SCRIPT_FILE = fopen(script_path, "r"))) {
@@ -173,13 +186,19 @@ void _handle_get_input(ssize_t chars) {
 // Frees all manually allocated memory
 void _cleanup() {
     free(CWD);
+    CWD = NULL;
+
     free(INPUT_BUFFER);
+    INPUT_BUFFER = NULL;
+
     vector_destroy(CMD_HIST);
-    free(HISTORY_FILE);
-    free(SCRIPT_FILE);
-    if (INPUT_STREAM != stdin) {
-        free(INPUT_STREAM);
-    }
+    CMD_HIST = NULL;
+
+    if (HISTORY_FILE) fclose(HISTORY_FILE);
+    HISTORY_FILE = NULL;
+
+    if (SCRIPT_FILE) fclose(SCRIPT_FILE);
+    SCRIPT_FILE = NULL;
 }
 
 // Validates that the options passed into argv are valid and expected
@@ -219,45 +238,16 @@ bool _validate_options(int argc, char * argv[], char ** hist_path, char ** scrip
     return true;
 }
 
-/* will run builtin command if command is a built in
- * -1 if builtin  was not run
- * NULL if builtin run, but no command is to be saved
- * Other strings will be command to save
-*/
-bool _run_builtin(vector * args) {
-    char path[PATH_MAX];
-    char prefix[PATH_MAX];
-    size_t n = 0;
-    
-
+// returns whether or not a builtin function ran
+bool _run_builtin(vector * args) {  
     if (vector_size(args) == 0) return false;
     if (_handle_history(args)) return true;
     if (_handle_prev_command(args)) return true;
-    else if (sscanf(command, "#%zu", &n) == 1) {
-        if (n >= vector_size(CMD_HIST)) {
-            print_invalid_index();
-        } else  {
-            //char * history_command = (char *)*vector_at(CMD_HIST, n);
-            // TODO: EXECUTE COMMAND HERE
-        }
-        return true;
-    } else if (sscanf(command, "!%s", prefix) == 1) {
-        return _run_prefix(prefix);
-        return true;
-    }
-    if (sscanf(INPUT_BUFFER, "cd %s", path) == 1) {
-        if (chdir(path) == -1) {
-            // TODO: Maybe base printing off of the specific errno
-            print_no_directory(path);
-        } else {
-            free(CWD);
-            CWD = NULL;
-            CWD = get_full_path(".");
-        }   
-        
-        return "1";
-    }
+    if (_handle_cd(args)) return true;
+    if (_handle_prefix(args)) return true;
 
+    print_invalid_command(INPUT_BUFFER); // TODO: Is this print correct?
+    _add_to_history(INPUT_BUFFER);
     return false;
 }
 
@@ -268,42 +258,11 @@ void _print_history() {
     }
 }
 
-char * _run_prefix(char * prefix) {
-    size_t prefix_len = strlen(prefix);
-    size_t i;
-    for (i = vector_size(CMD_HIST) - 1; i >= 0; i--) {
-        char * command = (char *)*vector_at(CMD_HIST, i);
-        char * prefix_compare = malloc(prefix_len + 1);
-        strncpy(prefix_compare, command, prefix_len);
-        prefix_compare[prefix_len] = '\0';
-
-        bool match = !strcmp(prefix, prefix_compare);
-        free(prefix_compare);
-        prefix_compare = NULL;
-        if (match) {
-            // TODO: EXECUTE command
-            return command;
-        }
-    }
-
-    print_no_history_match();
-    return  NULL;
-}
-
-// char * _run_command(vector * args) {
-//     // TODO: DOES NOT WORK, NEED list of args
-//     char * to_save = _run_builtin(args);
-//     if (to_save != (char *)-1) {
-//         return to_save;
-//     }
-
-//     // command was not builtin, run with fork exec wait
-//     return NULL;
-// }
-
 vector * _split_input(char * command) {
     vector * command_args = string_vector_create();
-    char * arg = strtok(command, " ");
+    char * command_copy = malloc(strlen(command) + 1);
+    strcpy(command_copy, command);
+    char * arg = strtok(command_copy, " ");
     if (arg) {
         vector_push_back(command_args, arg);
     }
@@ -311,14 +270,7 @@ vector * _split_input(char * command) {
     while ((arg = strtok(NULL, " "))) {
         vector_push_back(command_args, arg);
     }
-
-    vector_push_back(command_args, NULL);
-    
-    // TODO: remove this
-    size_t i;
-    for (i = 0; i < vector_size(command_args); i++) {
-        fprintf(stderr, "%s\n", (char*)*vector_at(command_args, i));
-    }  
+    free(command_copy);
     return command_args;
 }
 
@@ -326,8 +278,15 @@ vector * _split_input(char * command) {
 void _exit_success() {
     size_t i;
     if (RECORD_HIST) {
+        fprintf(stderr, "Writing to: %p\n",HISTORY_FILE);
         for (i = 0; i < vector_size(CMD_HIST); i++) {
-            fputs((char *)*vector_at(CMD_HIST, i), HISTORY_FILE);
+            char * command = (char *)*vector_at(CMD_HIST, i);
+            // char * newline = strchr(command, '\n');
+            // if (!newline) {
+            //     command[strlen(command)] = '\n';
+            // }
+
+            fputs(command, HISTORY_FILE);
         }
     }
 
@@ -358,17 +317,78 @@ bool _handle_prev_command(vector * args) {
     if (vector_size(args) == 1) {
         int n;
         char * command = (char *)*vector_at(args, 0);
-        if (sscanf(command "#%d", n) == 1) {
-            if (n < 0 || n >= vector_size(CMD_HIST)) {
+        if (sscanf(command, "#%d", &n) == 1) {
+            if (n < 0 || (size_t) n >= vector_size(CMD_HIST)) {
                 print_invalid_index();
             } else {
                 char * history_command = (char *)*vector_at(CMD_HIST, n);
                 // TODO: EXECUTE history_command
-                vector_push_back(CMD_HIST, history_command);
+                _add_to_history(history_command);
             }
             return true;
         }
     }
 
     return false;
+}
+
+bool _handle_cd(vector * args) {
+    // TODO: change operation depending on '/' as first char or not
+    if (vector_size(args) == 2) {
+        char * cd = (char *)*vector_at(args, 0);
+        char * path = (char *)*vector_at(args, 1);
+
+        if (strcmp(cd, "cd") == 0) {
+            if (chdir(path) == -1) {
+                // TODO: maybe only print if specifically invalid path errno is given
+                print_no_directory(path);
+            }
+
+            free(CWD);
+            CWD = NULL;
+            CWD = get_full_path(".");
+            _add_to_history(INPUT_BUFFER); // adding input buffer because not segmented
+            return true;
+        }
+    }
+    return false;
+}
+
+bool _handle_prefix() {
+    char prefix[strlen(INPUT_BUFFER)];
+
+    if (sscanf(INPUT_BUFFER, "!%s", prefix) == 1) {
+        size_t prefix_len = strlen(prefix);
+        char * prefix_compare = malloc(sizeof(prefix));
+        size_t i;
+        bool match = false;
+
+        if (vector_size(CMD_HIST) > 0) {
+            for (i = vector_size(CMD_HIST) - 1; i >= 0; i--) {
+                char * history_command = (char *)*vector_at(CMD_HIST, i);
+                prefix_compare[0] = '\0';
+                strncpy(prefix_compare, history_command, prefix_len);
+                prefix_compare[prefix_len] = '\0';
+                
+                if (!strcmp(prefix, prefix_compare)) {
+                    // TODO: EXECUTE command
+                    _add_to_history(history_command);
+                    match = true;
+                    break;
+                }
+            }
+        }
+
+        // no matches
+        if (!match) print_no_history_match();
+        return true;
+    }
+    return false;
+}
+
+void _add_to_history(char * command) {
+    if (command[CHARS_READ - 1] == '\0') {
+        command[CHARS_READ - 1] = '\n';
+    }
+    vector_push_back(CMD_HIST, command);
 }
