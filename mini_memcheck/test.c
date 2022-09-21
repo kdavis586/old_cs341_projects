641bb7bc5d51185b3aa24d651af05d241c7a197a
Kaelan Davis <kaeland2@illinois.edu>
2022-09-21 11:16:25 -0500

    Ran another test, everything looks good

--- mini_memcheck/test.c
+++ mini_memcheck/test.c
@@ -10,19 +10,19 @@
 int main() {
     void *p1 = malloc(30); 
     void *p2 = malloc(40); 
-    void *p3 = malloc(50); 
-    p2 = realloc(p2, 10);  
-    p1 = realloc(p1, 100);
+    void *p3 = malloc(50); //req = 120
+    p2 = realloc(p2, 10);  // free = 30
+    p1 = realloc(p1, 100); // req = 190
     void *temp = realloc(p1, 1000000000000000);
     if (temp) {
         p1 = temp;
     }
-    free(p2); // should free 10
-    free(p1); // should free 100
-    p3 = realloc(p3, 0); //should free 50
-    int * int_arr = calloc(3, sizeof(int));
+    free(p2); // free = 40
+    free(p1); // free = 140
+    p3 = realloc(p3, 0); // free = 190
+    int * int_arr = calloc(3, sizeof(int)); // req = 202
     assert(!int_arr[1]);
-    free(int_arr);
+    free(int_arr); // free = 202
     free(int_arr);
     return 0;
 }
