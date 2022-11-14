563a778db56772be024add59526026214546a276
Kaelan Davis <kaeland2@illinois.edu>
2022-11-14 12:43:22 -0600

    created helper functions write_all and read_all

--- nonstop_networking/client.c
+++ nonstop_networking/client.c
@@ -13,11 +13,13 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <errno.h>
-
 #include "common.h"
 
 char **parse_args(int argc, char **argv);
 verb check_args(char **args);
+void write_all(int fd, char * buffer, size_t size);
+void read_all(int fd, char * buffer, size_t size);
+
 int main(int argc, char **argv) {
     // Good luck!
 
@@ -40,20 +42,21 @@ int main(int argc, char **argv) {
     int getaddr_res = getaddrinfo(host, port, &hints, &result);
     if (getaddr_res != 0) {
         // normally print out error here
-        // fprintf(stderr, "%s\n", gai_strerror(getaddr_res));
+        fprintf(stderr, "%s\n", gai_strerror(getaddr_res));// TODO remove
         exit(1);
     }
 
     int socket_fd = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
     if (socket_fd == -1) {
         //perror(NULL)
+        perror(NULL); // TODO: remove
         free(parsed_args);
         exit(1);
     }
 
     int connect_result = connect(socket_fd, result->ai_addr, result->ai_addrlen);
     if (connect_result == -1) {
-        //perror(NULL)
+        perror(NULL); // TODO: remove
         shutdown(socket_fd, SHUT_RDWR);
         close(socket_fd);
         print_connection_closed();
@@ -63,47 +66,31 @@ int main(int argc, char **argv) {
 
 
     switch (method) {
-        case GET:
+        case GET: {
             break;
-        case PUT:
+        }
+        case PUT: {
             break;
+        }
         case LIST: {
+            // structure LIST request
             char * write_buf = NULL;
             asprintf(&write_buf, "%s\n", str_method);
-            size_t written = 0;
 
-            while (written != strlen(write_buf)) {
-                ssize_t cur_written = write(socket_fd, write_buf + written, strlen(write_buf) - written);
-                if (cur_written == 0) {
-                    break;
-                } else if (cur_written > 0) {
-                    written += (size_t)cur_written;
-                } else if (cur_written == -1 && (errno == EINTR || errno == EAGAIN)) {
-                    continue;
-                } else {
-                    // error while writing
-                    exit(1);
-                }
-            }
+            // write request
+            write_all(socket_fd, write_buf, strlen(write_buf));
             free(write_buf);
             shutdown(socket_fd, SHUT_WR);
 
+            // prepare read buffer
             char read_buf[4096];
             memset(read_buf, 0, 4096);
-            size_t read_bytes = 0;
-            while (read_bytes < 4096) {
-                ssize_t cur_read = read(socket_fd, read_buf + read_bytes, 4096 - read_bytes);
-                if (cur_read == 0) {
-                    break;
-                } else if (cur_read > 0) {
-                    read_bytes += (size_t)cur_read;
-                } else if (cur_read == -1 && (errno == EINTR || errno == EAGAIN)) {
-                    continue;
-                } else {
-                    exit(1);
-                }
-            }
+            
+            // read from server
+            read_all(socket_fd, read_buf, (size_t)4096);
 
+            // write results from server
+            // TODO: this is not correct, need to parse size
             char * res = strtok(read_buf, "\n");
             while (res) {
                 fprintf(stdout, "%s", res);
@@ -112,13 +99,15 @@ int main(int argc, char **argv) {
 
             break;
         }
-        case DELETE:
+        case DELETE: {
             break;
-        default:
+        }
+        default: {
             shutdown(socket_fd, SHUT_RDWR);
             close(socket_fd);
             free(parsed_args);
             exit(1);
+        }
     }
 
     shutdown(socket_fd, SHUT_RDWR);
@@ -213,4 +202,38 @@ verb check_args(char **args) {
     // Not a valid Method
     print_client_help();
     exit(1);
+}
+
+// write all the bytes to the description, exit on failure
+void write_all(int fd, char * buffer, size_t size) {
+    size_t bytes_wrote = 0;
+    while (bytes_wrote != size) {
+        ssize_t cur_written = write(fd, buffer + bytes_wrote, size - bytes_wrote);
+        if (bytes_wrote == 0) {
+            return;
+        } else if (cur_written > 0) {
+            bytes_wrote += (size_t)cur_written;
+        } else if (cur_written == -1 && (errno == EINTR || errno == EAGAIN)) {
+            continue;
+        } else {
+            // error while writing
+            exit(1);
+        }
+    }
+}
+
+void read_all(int fd, char * buffer, size_t size) {
+    size_t bytes_read = 0;
+    while (bytes_read < size) {
+        ssize_t cur_read = read(fd, buffer + bytes_read, size - bytes_read);
+        if (cur_read == 0) {
+            return;
+        } else if (cur_read > 0) {
+            bytes_read += (size_t)cur_read;
+        } else if (cur_read == -1 && (errno == EINTR || errno == EAGAIN)) {
+            continue;
+        } else {
+            exit(1);
+        }
+    }
 }
